fee4e31dfda97c12054ed55e688a5f56f770964e
Ignas-rgb <61393113+Ignas-rgb@users.noreply.github.com>
2024-12-12 02:03:24 +0200

    update invalid filter

--- src/repositories/reports/report.repository.ts
+++ src/repositories/reports/report.repository.ts
@@ -51,23 +51,11 @@ export class ReportRepository {
     category?: ReportCategory,
   ): Promise<Report[]> {
     const query: any = { isDeleted: isDeleted };
-    const reports = await this.reportModel.find({
-      type: { $nin: ['trash', 'forest', 'beetle', 'permits'] },
+    const reports = await this.reportModel.deleteOne({
+      type: 'trash2'
     }).exec();
 
 
-    for(var i = 0; i < reports.length; i++) {
-      console.log(reports[i]);
-      console.log(reports[i].historyData);
-      for(var j=0; j<reports[i].historyData.length; j++) {
-          console.log('History data');
-        console.log(reports[i].historyData[j]);
-        for(var k=0; k <reports[i].historyData[j].edits.length; k++) {
-          console.log(reports[i].historyData[j].edits[k]);
-        }
-      }
-      console.log(reports[i].statusRecords);
-    }
     return this.reportModel.find(query).sort({ reportDate: -1 }).exec();
   }
 
